9f027bf304c1d8b14e693269e8ff49010cb82c98
Tanishq <tanishqraj68@gmail.com>
2024-01-15 03:01:59 +0530

    commit

---
+++ script.js
@@ -0,0 +1,182 @@
+const menuItems=document.querySelectorAll('.shoe-type')
+const wrapper=document.querySelector('.sliderWrapper')
+
+
+
+
+
+const shoes=[
+{
+name:"Air Force",
+price:"Rs.3999",
+details:"This are Air Jordans",
+
+colors:[{code:'black',
+img:'air.png'}
+    
+    
+    ,{code:'blue',
+    img:'air2.png'}]
+
+
+
+},
+
+
+{
+    name:"Jordans",
+    price:"Rs.3999",
+    details:"This are Air Jordans",
+    
+    colors:[{code:'grey',
+    img:'jordan.png'},
+        
+        
+        {code:'teal',
+        img:'jordan2.png'}]
+    
+    
+},
+
+
+{
+    name:"Blazer",
+    price:"Rs.3999",
+    details:"This are Air Jordans",
+    
+    colors:[{code:'grey',
+    img:'blazer.png'},
+        
+        
+        {code:'green',
+        img:'blazer2.png'}]
+    
+    
+},
+
+{
+    name:"Crafter",
+    price:"Rs.3999",
+    details:"This are Air Jordans",
+    
+    colors:[{code:'black',
+    img:'crater.png'},
+        
+        
+       {code:'grey',
+        img:'crater2.png'}]
+    
+    
+},
+
+
+{
+    name:"Hippie",
+    price:"Rs.3999",
+    details:"This are Air Jordans",
+    
+    colors:[{
+        code:'grey',
+    img:'hippie.png'},
+        
+        
+        {code:'black',
+        img:'hippie2.png'}]
+    
+    
+}
+]
+
+let currenShoe=shoes[0];
+
+
+const productImage=document.querySelector('.product-image')
+const productPrice=document.querySelector('.product-price')
+const productDetails=document.querySelector('.product-details')
+const productName=document.querySelector('.product-name')
+
+const colors=document.querySelectorAll('.color')
+
+const productSection=document.querySelector('.product')
+const buyButton=document.querySelector('.buy-button')
+
+const sizes=document.querySelectorAll('.size')
+
+const productBtn=document.querySelector('.product-btn')
+
+const closePayment=document.querySelector('.close')
+
+const payment=document.querySelector('.payment')
+const blurEffect=document.querySelector('.blur-div')
+
+
+menuItems.forEach((el,index)=>{
+
+    el.addEventListener('click',function(e){
+       currenShoe=shoes[index]
+    //    console.log(currenShoe.colors[0].code);
+    wrapper.style.transform=`translateX(${-100*index}vw`
+
+
+
+    productImage.src=shoes[index].colors[0].img
+    productPrice.textContent=`$ ${shoes[index].price}`
+    productDetails.textContent=`${shoes[index].details}`
+    productName.textContent=`${shoes[index].name}`
+
+
+    colors.forEach((el,index)=>{
+        el.style.backgroundColor=`${currenShoe.colors[index].code}`
+    })
+    })
+    })
+
+
+    buyButton.addEventListener('click',function(){
+        productSection.scrollIntoView({behavior:'smooth'})
+    })
+
+
+    colors.forEach((el,i)=>{
+        el.addEventListener('click',function(){
+            console.log(currenShoe);
+            productImage.src=currenShoe.colors[i].img
+        })
+    })
+
+
+const prodSizes=document.querySelectorAll('.size')
+
+prodSizes.forEach(element => {
+    
+element.addEventListener('click',function(){
+    prodSizes.forEach(el=>{
+        el.style.backgroundColor='white'
+        el.style.color='black'
+    })
+    
+    element.style.backgroundColor='black'
+    element.style.color='white'
+})
+
+   
+});
+
+
+productBtn.addEventListener('click',function(){
+payment.style.display='flex'
+blurEffect.style.display='block'
+payment.scrollIntoView({behavior:'smooth'})
+
+})
+
+
+closePayment.addEventListener('click',function(){
+    payment.style.display='none'
+blurEffect.style.display='none'
+})
+
+
+    
+    
+ 
